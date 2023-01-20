2e8aabfc98830759e0d323bee294c9a3c68abc8a
dskim731 <dskim731@gmail.com>
2023-01-19 18:46:44 -0600

    main page edit

--- game.py
+++
@@ -1,9 +0,0 @@
-from player import Player, Dealer
-
-
-player_score = 0
-dealer_score = 0
-
-def deal_cards(player, dealer):
-    print("Welcome to Blackjack!")
-    user_input = input("Press any key to start the game.").lower()
--- main.py
+++ main.py
@@ -3,33 +3,84 @@ import random
 from PIL import Image, ImageTk
 
 
-# Game board display
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Blackjack")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__root.geometry("1200x800")
-        self.__root.configure(background="green")
-        
+root = Tk()
+root.title("Blackjack")
+canvas = Canvas(root, bg="green", height=1200, width=800)
+canvas.pack(fill=BOTH, expand=1)
 
-class Cards:
-    def __init__(self):
-        pass
-    
-    # Resize Cards
-    def resize_cards(card):
-        # Open the Image
-        card_img = Image.open(card)
+img = ImageTk.PhotoImage(Image.open("gui/images/cards/2_of_clubs.png"))
+canvas.create_image(0, 0, image=img, anchor="nw")
 
-        # Resize The Image
-        card_resize_image = card_img.resize((150, 218))
-        
-        global card_image
-        card_image = ImageTk.PhotoImage(card_resize_image)
 
-        return card_image
-    
-class Chips:
-    def __init__(self):
-        pass
+def shuffle():
+    suits = ["diamonds", "clubs", "hearts", "spades"]
+    values = range(2, 15)
+
+    global deck
+    deck = []
+
+    for suit in suits:
+        for value in values:
+            deck.append(f'{value}_of_{suit}')
+
+    global dealer, player
+    dealer = []
+    player = []
+
+    # Grab a random Card For Dealer
+    card = random.choice(deck)
+    # Remove Card From Deck
+    deck.remove(card)
+    # Append Card To Dealer List
+    dealer.append(card)
+    # Output Card To Screen
+    global dealer_image
+    dealer_image = resize_cards(f'images/cards/{card}.png')
+    dealer_label.config(image=dealer_image)
+
+    # Grab a random Card For Player
+    card = random.choice(deck)
+    # Remove Card From Deck
+    deck.remove(card)
+    # Append Card To Dealer List
+    player.append(card)
+    # Output Card To Screen
+    global player_image
+    player_image = resize_cards(f'images/cards/{card}.png')
+    player_label.config(image=player_image)
+
+    root.title(f'{len(deck)} Cards Left')
+
+
+def deal_cards():
+    try:
+        # Dealer card
+        card = random.choice(deck)
+        deck.remove(card)
+        # Append Card To Dealer List
+        dealer.append(card)
+        # Output Card To Screen
+        global dealer_image
+        dealer_image = resize_cards(f'images/cards/{card}.png')
+        dealer_label.config(image=dealer_image)
+
+        # Player Card
+        card = random.choice(deck)
+        # Remove Card From Deck
+        deck.remove(card)
+        # Append Card To Dealer List
+        player.append(card)
+        # Output Card To Screen
+        global player_image
+        player_image = resize_cards(f'images/cards/{card}.png')
+        player_label.config(image=player_image)
+        # player_label.config(text=card)
+
+        # Put number of remaining cards in title bar
+        root.title(f'{len(deck)} Cards Left')
+
+    except:
+        root.title(f'No Cards In Deck')
+
+
+root.mainloop()
