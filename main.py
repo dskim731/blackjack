b5d1af1f13ae749e813398b6a2d8f8e40d7f70d6
dskim731 <dskim731@gmail.com>
2023-02-02 18:59:30 -0600

    player class

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import random
+from random import shuffle
 
-
+# Window for the game
 root = Tk()
 root.title('Blackjack')
 root.geometry("900x500")
 root.configure(background="green")
 
+# Frame for the cards
+my_frame = Frame(root, bg="green")
+my_frame.pack(pady=20)
 
-# List for Dealer and Player hands
-dealer = []
-player = []
+# Dealer frame
+dealer_frame = LabelFrame(my_frame, text="Dealer", bd=0)
+dealer_frame.grid(row=0, column=0, padx=20, ipadx=20)
 
-# Function to resize the card images
+# Player frame
+player_frame = LabelFrame(my_frame, text="Player", bd=0)
+player_frame.grid(row=0, column=1, ipadx=20)
+
+dealer_label = Label(dealer_frame, text='')
+dealer_label.pack(pady=20)
+
+player_label = Label(player_frame, text='')
+player_label.pack(pady=20)
+
+# Shuffle buttons
+shuffle_button = Button(root, text="Shuffle Deck",
+                        font=("Helvetica", 14), command=shuffle)
+shuffle_button.pack(pady=20)
+card_button = Button(root, text="Get Cards", font=(
+    "Helvetica", 14), command=deal_cards)
+card_button.pack(pady=20)
+
+# Card resizing
 
 
 def resize_cards(self, card):
@@ -21,28 +43,49 @@ def resize_cards(self, card):
     card_img = Image.open(card)
     # Resize image
     card_resize_image = card_img.resize((150, 218))
-    # Card
+    # Display card
     card_image = ImageTk.PhotoImage(card_resize_image)
     # Return card
     return card_image
 
 
-# Shuffle Cards
-def shuffle():
-    suits = ["diamonds", "clubs", "hearts", "spades"]
-    values = range(2, 15)
-    # 11 = Jack, 12=Queen, 13=King, 14 = Ace
-
-    # List for the deck of cards
-    global deck
-    deck = []
-
-    for suit in suits:
-        for value in values:
-            deck.append(f'{value}_of_{suit}')
+# Suits and values for the cards
+suits = ["diamonds", "clubs", "hearts", "spades"]
+values = range(2, 15)
+# 11 = Jack, 12=Queen, 13=King, 14 = Ace
 
 
-# Deal Out Cards
+class Card():
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    # Print card
+    def __str__(self):
+        print(f"{self.value} of {self.suit}")
+
+
+class Deck():
+    def __init__(self):
+        self.deck = []
+
+        for suit in suits:
+            for value in values:
+                self.deck.append(f'{value}_of_{suit}')
+
+    def shuffle_deck(self):
+        return shuffle(self.deck)
+
+    def deal_card(self):
+        self.deck.pop(0)
+
+
+class Player():
+    def __init__(self, chips):
+        self.chips = chips
+
+
+"""
 def deal_cards(self):
     try:
         # Get the Dealer Card
@@ -74,37 +117,7 @@ def deal_cards(self):
 
     except:
         root.title(f'No Cards In Deck')
+"""
 
-
-# Frame
-my_frame = Frame(root, bg="green")
-my_frame.pack(pady=20)
-
-# Create Frames For Cards
-dealer_frame = LabelFrame(my_frame, text="Dealer", bd=0)
-dealer_frame.grid(row=0, column=0, padx=20, ipadx=20)
-
-player_frame = LabelFrame(my_frame, text="Player", bd=0)
-player_frame.grid(row=0, column=1, ipadx=20)
-
-# Put cards in frames
-dealer_label = Label(dealer_frame, text='')
-dealer_label.pack(pady=20)
-
-player_label = Label(player_frame, text='')
-player_label.pack(pady=20)
-
-# Create a couple buttons
-shuffle_button = Button(root, text="Shuffle Deck",
-                        font=("Helvetica", 14), command=shuffle)
-shuffle_button.pack(pady=20)
-
-card_button = Button(root, text="Get Cards", font=(
-    "Helvetica", 14), command=deal_cards)
-card_button.pack(pady=20)
-
-
-# Shuffle Deck On Start
-shuffle()
 
 root.mainloop()
