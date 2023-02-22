bafd7921df187c7cd2af437554ec3bd407d35574
dskim731 <dskim731@gmail.com>
2023-02-21 23:54:25 -0600

    new structure

--- main.py
+++ main.py
@@ -1,63 +1,136 @@
 from tkinter import *
-import tkinter as tk
-from PIL import Image, ImageTk
 import random
+from PIL import Image, ImageTk
 
 
-class Window(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        self.root = Tk()
-        self.root.title('Blackjack')
-        self.root.geometry("900x500")
-        self.root.configure(background="green")
-
-        # Frames for the cards
-        container = tk.Frame(self.root, bg="black", height=400, width=600)
-        container.pack(pady=20)
+root = Tk()
+root.title("Blackjack")
+root.geometry("900x500")
+root.configure(background="green")
 
 
-class Dealer(tk.Frame):
-    def __init__(self):
-        tk.Frame.__init__(self)
-        # Dealer frame
-        dealer_frame = LabelFrame(container=True, text="Dealer", bd=0)
-        dealer_frame.grid(row=0, column=0, padx=20, ipadx=20)
+def resize_cards(card):
+    # Open the image
+    my_card_img = Image.open(card)
 
-        # Dealer label
-        dealer_label = tk.Label(self)
-        dealer_label.pack(pady=20)
+    # Resize image
+    card_resize_image = my_card_img.resize((150, 218))
 
+    # Display card image
+    global card_image
+    card_image = ImageTk.PhotoImage(card_resize_image)
 
-class Player(tk.Frame):
-    def __init__(self):
-        tk.Frame.__init__(self)
-        # Player frame
-        player_frame = LabelFrame(container=True, text="Player", bd=0)
-        player_frame.grid(row=0, column=0, padx=20, ipadx=20)
-
-        # Player label
-        player_label = tk.Label(self)
-        player_label.pack(pady=20)
+    return card_image
 
 
-# Suits and values for the cards
-suits = ["diamonds", "clubs", "hearts", "spades"]
-values = range(2, 15)
-# 11=Jack, 12=Queen, 13=King, 14=Ace
+def shuffle():
+    # Deck function
+    suits = ["diamonds", "clubs", "hearts", "spades"]
+    values = range(2, 15)
+    # 11=Jack, 12=Queen, 13=King, 14=Ace
+
+    global deck
+    deck = []
+
+    for suit in suits:
+        for value in values:
+            deck.append(f'{value}_of_{suit}')
+
+    # Create player and dealer
+    global dealer, player
+    dealer = []
+    player = []
+
+    # Dealer deal card
+    card = random.choice(deck)
+    # Remove Card From Deck
+    deck.remove(card)
+    # Append Card To Dealer List
+    dealer.append(card)
+    # Output Card To Screen
+    global dealer_image
+    dealer_image = resize_cards(f'images/{card}.png')
+    dealer_label.config(image=dealer_image)
+
+    # Deal player card
+    card = random.choice(deck)
+    # Remove Card From Deck
+    deck.remove(card)
+    # Append Card To Dealer List
+    player.append(card)
+    # Output Card To Screen
+    global player_image
+    player_image = resize_cards(f'images/{card}.png')
+    player_label.config(image=player_image)
+
+    # Number of cards left in the deck
+    root.title(f'{len(deck)} cards left')
 
 
-class Card():
-    def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
+# Deal Out Cards
+def deal_cards():
+    try:
+        # Get the Dealer Card
+        card = random.choice(deck)
+        # Remove Card From Deck
+        deck.remove(card)
+        # Append Card To Dealer List
+        dealer.append(card)
+        # Output Card To Screen
+        global dealer_image
+        dealer_image = resize_cards(f'images/{card}.png')
+        dealer_label.config(image=dealer_image)
+        # dealer_label.config(text=card)
 
-    def resize_cards(card):
-        # Open image
-        card_img = Image.open(card)
-        # Resize image
-        card_resize_image = card_img.resize((150, 218))
-        # Display card
-        card_image = ImageTk.PhotoImage(card_resize_image)
-        # Return card
-        return card_image
+        # Get the Player Card
+        card = random.choice(deck)
+        # Remove Card From Deck
+        deck.remove(card)
+        # Append Card To Dealer List
+        player.append(card)
+        # Output Card To Screen
+        global player_image
+        player_image = resize_cards(f'images/{card}.png')
+        player_label.config(image=player_image)
+        # player_label.config(text=card)
+
+        # Put number of remaining cards in title bar
+        root.title(f'{len(deck)} Cards Left')
+
+    except:
+        root.title(f'No Cards In Deck')
+
+
+my_frame = Frame(root, bg="green")
+my_frame.pack(pady=20)
+
+# Create Frames For Cards
+dealer_frame = LabelFrame(my_frame, text="Dealer", bd=0)
+dealer_frame.grid(row=0, column=0, padx=20, ipadx=20)
+
+player_frame = LabelFrame(my_frame, text="Player", bd=0)
+player_frame.grid(row=0, column=1, ipadx=20)
+
+# Put cards in frames
+dealer_label = Label(dealer_frame, text='')
+dealer_label.pack(pady=20)
+
+player_label = Label(player_frame, text='')
+player_label.pack(pady=20)
+
+
+# Create a couple buttons
+shuffle_button = Button(root, text="Shuffle Deck",
+                        font=("Arial", 14), command=shuffle)
+shuffle_button.pack(pady=20)
+
+card_button = Button(root, text="Get Cards", font=(
+    "Arial", 14), command=deal_cards)
+card_button.pack(pady=20)
+
+
+# Shuffle Deck On Start
+shuffle()
+
+
+root.mainloop()
